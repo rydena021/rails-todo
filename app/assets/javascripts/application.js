c0beea75737bb549ded04544e883b24711371849
Anders Ryden <rydena021@gmail.com>
2017-04-07 07:15:34 -0500

    sidebar toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
